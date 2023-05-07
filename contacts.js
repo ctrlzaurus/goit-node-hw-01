00b70a4cf15c98e6f756eb0f3974491e64b0b888
ctrlzaurus <ichi.di@icloud.com>
2023-05-07 22:06:48 +0300

    new hw 1

---
+++ contacts.js
@@ -0,0 +1,43 @@
+const { v4: uuidv4 } = require("uuid");
+
+const fs = require('fs').promises;
+const path = require('path');
+const { writeFile } = require("fs");
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+// TODO: задокументувати кожну функцію
+async function listContacts() {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const contact = contacts.find(item => item.id === contactId) || null;
+    return contact;
+}
+
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const i = contacts.findIndex((item) => item.id === contactId);
+    if (i === -1) return null;
+    const reContact = contacts.splice(i, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return reContact[0];
+}
+
+async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+    const newContact = {
+        id: uuidv4(),
+        name, 
+        email, 
+        phone
+    }
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+}
+
+module.exports = {listContacts, getContactById, removeContact, addContact};
---
+++ index.js
@@ -0,0 +1,44 @@
+const {listContacts, getContactById, addContact, removeContact} = require('./contacts');
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+async function invokeAction ({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const list = await listContacts();
+      console.table(list);
+      break;
+
+    case "get":
+      const get = await getContactById(id);
+      console.log(get);
+      break;
+
+    case "add":
+      const add = await addContact(name, email, phone);
+      console.log([add]);
+      break;
+
+    case "remove":
+      const remove = await removeContact(id);
+      console.log(remove);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
